46fd687a38422824390dbe1f91704c475ff112bc
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-08-12 19:27:55 -0500

    Tests del scope global y el scope local

---
+++ src/scope/global.js
@@ -0,0 +1,17 @@
+var hello = 'hello';
+let world = 'Hello, World';
+const bye = 'Bye, World';
+
+console.log(hello);
+console.log(world);
+console.log(bye);
+
+const aFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(bye);
+}
+
+aFunction();
+ 
+
