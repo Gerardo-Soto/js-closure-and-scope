7c153aba6533d93094d45ea6610fa2a90d187454
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-08-14 18:22:50 -0500

    Utilizando Closures para crear variables privadas mediante los métodos get y set

--- closure/private.js
+++ closure/private.js
@@ -1,1 +1,22 @@
 // Private
+// metodos internos para obtener el valor de una variable o 
+// modificarla, pero nunca vamos a poder hacerlo desde afuera
+
+const person = () => {
+    var saveName = "Name";
+    return {
+        getName: () => {
+            return saveName; 
+        },
+        setName: (name) => {
+            saveName = name;
+        } 
+    }
+}
+
+newPerson = person();
+console.log(newPerson.getName());
+newPerson.setName('Gerard');
+console.log(newPerson.getName());
+
+
